70bca9cfdc730936f4b2f6dda650376aaa3ae5c6
Freddi Anton Sitorus <Freddi Anton Sitorus@BAT04109L.inmarsat.local>
2019-07-22 17:20:17 +0700

    New project

---
+++ PHPTravel/src/main/java/com/phptravel/PHPTravel/App.java
@@ -0,0 +1,13 @@
+package com.phptravel.PHPTravel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
